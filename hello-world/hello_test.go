e634510d2d996006364d119c0b64976c62270678
John Mayou <john@johnmayou.com>
2024-12-20 15:57:12 -0600

    add additional name assertions

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -5,6 +5,8 @@ import "testing"
 func TestHello(t *testing.T) {
 	t.Run("returns greeting to passed name", func(t *testing.T) {
 		assertMessage(t, Hello("John"), "Hello, John")
+		assertMessage(t, Hello("Jane"), "Hello, Jane")
+		assertMessage(t, Hello("First Last"), "Hello, First Last")
 	})
 	t.Run("defaults to 'Hello, World' if empty name is passed", func(t *testing.T) {
 		assertMessage(t, Hello(""), "Hello, World")

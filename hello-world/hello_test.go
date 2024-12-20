f5bc226684796d9b5f33ecd12c4ddf9e84fc0f6f
John Mayou <john@johnmayou.com>
2024-12-20 15:56:46 -0600

    pull out assertion

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,19 +4,16 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("returns greeting to passed name", func(t *testing.T) {
-		got := Hello("John")
-		want := "Hello, John"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertMessage(t, Hello("John"), "Hello, John")
 	})
 	t.Run("defaults to 'Hello, World' if empty name is passed", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertMessage(t, Hello(""), "Hello, World")
 	})
+}
+
+func assertMessage(t testing.TB, got string, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }

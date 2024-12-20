5df2cbb1501dac7a007700b862ab281f68fe55bc
John Mayou <john@johnmayou.com>
2024-12-20 15:42:44 -0600

    default to World for name

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("John")
-	want := "Hello, John"
+	t.Run("returns greeting to passed name", func(t *testing.T) {
+		got := Hello("John")
+		want := "Hello, John"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("defaults to 'Hello, World' if empty name is passed", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }

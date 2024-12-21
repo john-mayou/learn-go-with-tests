5bd010897877175e079966eb112398a3fff5ea2f
John Mayou <john@johnmayou.com>
2024-12-21 14:21:57 -0600

    use table tests

--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -1,20 +1,48 @@
 package shapes
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
-	assertEqualFloat64(t, Perimeter(3.0, 4.0), 14.0)
-	assertEqualFloat64(t, Perimeter(10.0, 10.0), 40.0)
+	tests := []struct {
+		width    float64
+		height   float64
+		expected float64
+	}{
+		{width: 3.0, height: 4.0, expected: 14.0},
+		{width: 10.0, height: 10.0, expected: 40.0},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("returns expected perimeter for width %v and height %v", tt.width, tt.height)
+		t.Run(testname, func(t *testing.T) {
+			actual := Perimeter(tt.width, tt.height)
+			if actual != tt.expected {
+				t.Errorf("expected %.2f but got %.2f", tt.expected, actual)
+			}
+		})
+	}
 }
 
 func TestArea(t *testing.T) {
-	assertEqualFloat64(t, Area(3.0, 4.0), 12.0)
-	assertEqualFloat64(t, Area(10.0, 10.0), 100.0)
-}
+	tests := []struct {
+		width    float64
+		height   float64
+		expected float64
+	}{
+		{width: 3.0, height: 4.0, expected: 12.0},
+		{width: 10.0, height: 10.0, expected: 100.0},
+	}
 
-func assertEqualFloat64(t testing.TB, actual, expected float64) {
-	t.Helper()
-	if actual != expected {
-		t.Errorf("expected %.2f but got %.2f", expected, actual)
+	for _, tt := range tests {
+		testname := fmt.Sprintf("returns expected area for width %v and height %v", tt.width, tt.height)
+		t.Run(testname, func(t *testing.T) {
+			actual := Area(tt.width, tt.height)
+			if actual != tt.expected {
+				t.Errorf("expected %.2f but got %.2f", tt.expected, actual)
+			}
+		})
 	}
 }

27cef3882ccc5032181ce0a88e2a98fd5376938d
John Mayou <john@johnmayou.com>
2024-12-20 19:56:36 -0600

    add example and bench

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,6 +1,15 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func ExampleRepeat() {
+	output := Repeat("a")
+	fmt.Println(output)
+	// Output: aaaaa
+}
 
 func TestRepeat(t *testing.T) {
 	t.Run("returns string repeated 5 times", func(t *testing.T) {
@@ -13,5 +22,11 @@ func assertEqualStr(t testing.TB, actual, expected string) {
 	t.Helper()
 	if actual != expected {
 		t.Errorf("expected %q but got %q", expected, actual)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for range b.N {
+		Repeat("a")
 	}
 }

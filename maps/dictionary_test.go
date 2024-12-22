e23022c99a8a92e48f432d59e17def9f838a41ae
John Mayou <109235738+john-mayou@users.noreply.github.com>
2024-12-21 21:33:38 -0600

    Maps (#7)
    * initial dictionary
    
    * name Dictionary type
    
    * prefer attached method
    
    * wrap in test run
    
    * return err on unknown word
    
    * factor out error
    
    * add Add method
    
    * return err if word already exists
    
    * move errors together
    
    * prefer using Search to lookup
    
    * make errors constants
    
    * add Update
    
    * assert err is nil
    
    * prefer fatal since there was an error
    
    * add Delete

---
+++ maps/dictionary_test.go
@@ -0,0 +1,93 @@
+package dictionary
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	t.Run("known word", func(t *testing.T) {
+		dict := Dictionary{"word": "meaning"}
+		meaning, err := dict.Search("word")
+		assertNoErr(t, err)
+		assertEqual(t, meaning, "meaning")
+	})
+	t.Run("unknown word", func(t *testing.T) {
+		dict := Dictionary{"word": "meaning"}
+		_, err := dict.Search("random")
+		assertErr(t, err, ErrNotFound.Error())
+	})
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("add new word", func(t *testing.T) {
+		dict := Dictionary{}
+		err := dict.Add("word", "meaning")
+		assertNoErr(t, err)
+		meaning, err := dict.Search("word")
+		assertNoErr(t, err)
+		assertEqual(t, meaning, "meaning")
+	})
+	t.Run("does not override word", func(t *testing.T) {
+		dict := Dictionary{"word": "old meaning"}
+		err := dict.Add("word", "new meaning")
+		assertErr(t, err, ErrWordExists.Error())
+		meaning, err := dict.Search("word")
+		assertNoErr(t, err)
+		assertEqual(t, meaning, "old meaning")
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("updates word", func(t *testing.T) {
+		dict := Dictionary{"word": "old meaning"}
+		err := dict.Update("word", "new meaning")
+		assertNoErr(t, err)
+		meaning, err := dict.Search("word")
+		assertNoErr(t, err)
+		assertEqual(t, meaning, "new meaning")
+	})
+	t.Run("returns error on unknown word", func(t *testing.T) {
+		dict := Dictionary{}
+		err := dict.Update("word", "new meaning")
+		assertErr(t, err, ErrNotFound.Error())
+		_, err = dict.Search("word")
+		assertErr(t, err, ErrNotFound.Error())
+	})
+}
+
+func TestDelete(t *testing.T) {
+	t.Run("delete word", func(t *testing.T) {
+		dict := Dictionary{"word": "meaning"}
+		err := dict.Delete("word")
+		assertNoErr(t, err)
+		_, err = dict.Search("word")
+		assertErr(t, err, ErrNotFound.Error())
+	})
+	t.Run("returns error on uknown word", func(t *testing.T) {
+		dict := Dictionary{}
+		err := dict.Delete("word")
+		assertErr(t, err, ErrNotFound.Error())
+	})
+}
+
+func assertEqual[V comparable](t testing.TB, got, want V) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v but wanted %v", got, want)
+	}
+}
+
+func assertErr(t testing.TB, err error, msg string) {
+	t.Helper()
+	if err == nil {
+		t.Fatal("wanted err but did not get one")
+	}
+	if err.Error() != msg {
+		t.Errorf("got %q but wanted %q", err.Error(), msg)
+	}
+}
+
+func assertNoErr(t testing.TB, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatalf("wanted no err but got %q", err.Error())
+	}
+}
---
+++ maps/go.mod
@@ -0,0 +1,3 @@
+module example.com/dictionary
+
+go 1.23.4

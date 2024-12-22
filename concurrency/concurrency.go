7fc5592416e3fd685eda746fdebe17348082c3f4
John Mayou <john@johnmayou.com>
2024-12-22 01:07:47 -0600

    perf: use go routines

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -2,11 +2,24 @@ package concurrency
 
 type WebsiteChecker func(string) bool
 
+type result struct {
+	url   string
+	valid bool
+}
+
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool, len(urls))
+	resultChannel := make(chan result, len(urls))
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func() {
+			resultChannel <- result{url: url, valid: wc(url)}
+		}()
+	}
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.url] = r.valid
 	}
 
 	return results

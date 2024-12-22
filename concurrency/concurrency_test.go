c392e139cc2305f23cda671063b1d4bf01864f8e
John Mayou <john@johnmayou.com>
2024-12-22 00:36:32 -0600

    add benchmark

--- concurrency/concurrency_test.go
+++ concurrency/concurrency_test.go
@@ -3,6 +3,7 @@ package concurrency
 import (
 	"maps"
 	"testing"
+	"time"
 )
 
 func mockWebsiteChecker(url string) bool {
@@ -23,5 +24,21 @@ func assertEqualMaps[K comparable, V comparable](t testing.TB, got, want map[K]V
 	t.Helper()
 	if !maps.Equal(got, want) {
 		t.Errorf("got %v but wanted %v", got, want)
+	}
+}
+
+func slowWebsiteChecker(url string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "url"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowWebsiteChecker, urls)
 	}
 }

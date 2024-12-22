4a0883bcbdb3de238cba29c23e2e8333f458b49f
John Mayou <john@johnmayou.com>
2024-12-22 03:01:51 -0600

    use select and routines to parallelize

--- select/racer_test.go
+++ select/racer_test.go
@@ -1,13 +1,47 @@
 package racer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	t.Run("returns fastest url", func(t *testing.T) {
-		slowURL := "http://www.facebook.com"
-		fastURL := "http://www.quii.dev"
-		assertEqual(t, Racer(slowURL, fastURL), fastURL)
-	})
+	fastestServerTests := []struct {
+		serverA *httptest.Server
+		serverB *httptest.Server
+		want    string
+	}{
+		{serverA: makeDelayedServer(0 * time.Millisecond), serverB: makeDelayedServer(10 * time.Millisecond), want: "A"},
+		{serverA: makeDelayedServer(10 * time.Millisecond), serverB: makeDelayedServer(0 * time.Millisecond), want: "B"},
+	}
+
+	for i, tt := range fastestServerTests {
+		t.Run("returns fastest server #"+strconv.Itoa(i), func(t *testing.T) {
+			defer tt.serverA.Close()
+			defer tt.serverB.Close()
+
+			got := Racer(tt.serverA.URL, tt.serverB.URL)
+
+			switch tt.want {
+			case "A":
+				assertEqual(t, got, tt.serverA.URL)
+			case "B":
+				assertEqual(t, got, tt.serverB.URL)
+			default:
+				t.Errorf("Invalid server wanted: %q", tt.want)
+			}
+		})
+	}
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
 }
 
 func assertEqual[V comparable](t testing.TB, got, want V) {

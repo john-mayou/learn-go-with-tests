3ff16a78318e19df92176826f9f5bacb130e29c4
John Mayou <john@johnmayou.com>
2024-12-21 19:31:27 -0600

    return err on unknown word

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,13 @@
 package dictionary
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	meaning, found := d[word]
+	if !found {
+		return "", errors.New("unknown word")
+	}
+	return meaning, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,7 +5,14 @@ import "testing"
 func TestSearch(t *testing.T) {
 	t.Run("known word", func(t *testing.T) {
 		dict := Dictionary{"word": "meaning"}
-		assertEqual(t, dict.Search("word"), "meaning")
+		meaning, err := dict.Search("word")
+		assertNoErr(t, err)
+		assertEqual(t, meaning, "meaning")
+	})
+	t.Run("unknown word", func(t *testing.T) {
+		dict := Dictionary{"word": "meaning"}
+		_, err := dict.Search("random")
+		assertErr(t, err, "unknown word")
 	})
 }
 
@@ -13,5 +20,22 @@ func assertEqual[V comparable](t testing.TB, got, want V) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %v but wanted %v", got, want)
+	}
+}
+
+func assertErr(t testing.TB, err error, msg string) {
+	t.Helper()
+	if err == nil {
+		t.Fatal("wanted err but did not get one")
+	}
+	if err.Error() != msg {
+		t.Errorf("got %q but wanted %q", err.Error(), msg)
+	}
+}
+
+func assertNoErr(t testing.TB, err error) {
+	t.Helper()
+	if err != nil {
+		t.Errorf("wanted no err but got %q", err.Error())
 	}
 }
